d407b99d93f6906ccded8a59dd7e877ae9e6e6fb
Apurv Jawle <apurv.jawle@pangea.cloud>
2024-11-12 14:49:22 -0800

    feat: ai guard a document transformer

---
+++ libs/community/langchain_community/document_transformers/pangea_text_guard.py
@@ -0,0 +1,89 @@
+import os
+from typing import Any, Optional, Sequence
+from pydantic import SecretStr
+
+from langchain_core._api import beta
+from langchain_core.documents import BaseDocumentTransformer, Document
+
+try:
+    from pangea import PangeaConfig
+    from pangea.services import AIGuard
+except ImportError as e:
+    raise ImportError(
+        "Cannot import pangea, please install `pip install pangea-sdk==5.2.0b2`."
+    ) from e
+
+@beta(message="Pangea AI Guard service is in beta. Subject to change.")
+class PangeaGuardTransformer(BaseDocumentTransformer):
+    """Guard documents to monitor, sanitize, and protect sensitive data using Pangea's AI Guard service.
+
+    Requirements:
+        - Environment variable ``PANGEA_AI_GUARD_TOKEN`` must be set,
+          or passed as a named parameter to the constructor.
+
+    Example:
+        .. code-block:: python
+
+            from langchain_community.document_transformers.pangea_text_guard import PangeaGuardTransformer, PangeaConfig
+
+            # Initialize parameters
+            pangea_token = SecretStr(os.getenv("PANGEA_AI_GUARD_TOKEN"))
+            config = PangeaConfig(domain="gcp.us.pangea.cloud")
+            recipe="pangea_prompt_guard"
+
+            pangea_guard_transformer = PangeaGuardTransformer(pangea_token=pangea_token, config_id="", config=config, recipe=recipe)
+            guarded_documents = pangea_guard_transformer.transform_documents(docs)
+    """
+
+    _client: AIGuard
+    _recipe: str
+
+    def __init__(
+        self,
+        pangea_token: Optional[SecretStr] = None,
+        config: PangeaConfig | None = None,
+        config_id: str | None = None,
+        pangea_token_env_key_name: str = "PANGEA_AI_GUARD_TOKEN",
+        recipe: str = "pangea_prompt_guard",
+    ) -> None:
+        """
+        Args:
+            pangea_token: Pangea Prompt Guard API token.
+            config_id: Pangea Prompt Guard configuration ID.
+            config: PangeaConfig object.
+            recipe: Pangea AI Guard recipe.
+        """
+                
+        if not pangea_token:
+            pangea_token = SecretStr(os.getenv(pangea_token_env_key_name, ""))
+
+        if not pangea_token or not pangea_token.get_secret_value() or pangea_token.get_secret_value() == "":
+            raise ValueError(f"'{pangea_token_env_key_name}' must be set or passed")
+        
+        self._recipe = recipe
+        self._client = AIGuard(token=pangea_token.get_secret_value(), config=config, config_id=config_id)
+
+    async def atransform_documents(
+        self, documents: Sequence[Document], **kwargs: Any
+    ) -> Sequence[Document]:
+        raise NotImplementedError
+
+    def transform_documents(
+        self, documents: Sequence[Document], **kwargs: Any
+    ) -> Sequence[Document]:
+        """
+        Guard documents to monitor, sanitize, and protect sensitive data 
+        using Pangea's AI Guard service.
+        """ 
+        
+        guarded_documents = []
+        for document in documents:
+            guarded = self._client.guard_text(document.page_content, recipe=self._recipe)
+
+            if not guarded.result:
+                raise AssertionError(f"Guard operation failed for document: {document}")
+
+            guarded_content = guarded.result.redacted_prompt or document.page_content
+            guarded_documents.append(document.model_copy(update={"page_content": guarded_content}))
+
+        return guarded_documents
---
+++ libs/community/tests/integration_tests/document_transformers/test_pangea_text_guard.py
@@ -0,0 +1,44 @@
+"""Integration test Pangea AI Guard transformer."""
+
+import os
+from pydantic import SecretStr
+
+from langchain_core.documents import Document
+from langchain_community.document_transformers.pangea_text_guard import PangeaGuardTransformer, PangeaConfig
+
+try:
+    from pangea.tools import (
+        TestEnvironment,
+        get_test_domain,
+        get_test_token,
+    )
+except ImportError as e:
+    raise ImportError(
+        "Cannot import pangea, please install `pip install pangea-sdk==5.2.0b2`."
+    ) from e
+
+def test_pangea_text_guard() -> None:
+    texts = [
+        "John Doe logged into the system from IP address 192.168.1.101.",
+        "The server located in Frankfurt, Germany is currently offline.",
+        "Alice's recent login was traced to IP 10.0.0.45.",
+        "My credit card number is 5555555555554444.",
+        "The headquarters are located at 1234 Elm Street, Springfield, USA.",
+    ]
+
+    docs = [Document(page_content=t) for t in texts]
+
+    # Initialize parameters
+    env = TestEnvironment.DEVELOP
+    config = PangeaConfig(domain=get_test_domain(env))
+    recipe="pangea_ingestion_guard"
+
+    pangea_guard_transformer = PangeaGuardTransformer(pangea_token=SecretStr(get_test_token(env)), config=config, recipe=recipe)
+    guarded_documents = pangea_guard_transformer.transform_documents(docs)
+
+    assert len(guarded_documents) == 5
+    assert guarded_documents[0].page_content == "<PERSON> logged into the system from IP address 192.168.1.101."
+    assert guarded_documents[1].page_content == "The server located in <LOCATION>, <LOCATION> is currently offline."
+    assert guarded_documents[2].page_content == "<PERSON>'s recent login was traced to IP 10.0.0.45."
+    assert guarded_documents[3].page_content == "My credit card number is ****************."
+    assert guarded_documents[4].page_content == "The headquarters are located at 1234 Elm Street, <LOCATION>, <LOCATION>."
